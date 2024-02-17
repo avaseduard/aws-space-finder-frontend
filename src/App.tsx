78348a6dcfdd8f4f6e0e4c84518d44a206823105
eduard <avas.eduard@gmail.com>
2024-02-17 09:31:56 +0200

    created navbar and paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,59 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import './App.css';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import { useState } from 'react';
+// import LoginComponent from './components/LoginComponent';
+// import { AuthService } from './services/AuthService';
+// import { DataService } from './services/DataService';
+// import CreateSpace from './components/spaces/CreateSpace';
+// import Spaces from './components/spaces/Spaces';
+
+// const authService = new AuthService();
+// const dataService = new DataService(authService);
 
 function App() {
+  const [userName, setUserName] = useState<string | undefined>(undefined);
+
+  const router = createBrowserRouter([
+    {
+      element: (
+        <>
+          <NavBar userName={userName} />
+          <Outlet />
+        </>
+      ),
+      children: [
+        {
+          path: '/',
+          element: <div>Hello world!</div>,
+        },
+        {
+          path: '/login',
+          // element: <LoginComponent authService={authService} setUserNameCb={setUserName} />,
+          element: <div>Login page</div>,
+        },
+        {
+          path: '/profile',
+          element: <div>Profile page</div>,
+        },
+        {
+          path: '/createSpace',
+          // element: <CreateSpace dataService={dataService} />,
+          element: <div>Create space</div>,
+        },
+        {
+          path: '/spaces',
+          // element: <Spaces dataService={dataService} />,
+          element: <div>Spaces</div>,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <>
-      <h1>Project is initiated</h1>
-    </>
+    <div className='wrapper'>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
