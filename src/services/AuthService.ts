a9487da7f280b345160052869dad8434f00c0f41
eduard <avas.eduard@gmail.com>
2024-02-19 07:44:33 +0200

    created login function

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,11 +1,34 @@
+import { type CognitoUser } from '@aws-amplify/auth';
+import { AuthStack } from '../../../space-finder/outputs.json';
+import { Amplify, Auth } from 'aws-amplify';
+
+const awsRegion = 'eu-central-1';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: false,
+    region: awsRegion,
+    userPoolId: AuthStack.SpaceUserPoolId,
+    userPoolWebClientId: AuthStack.SpaceUserPoolClientId,
+    identityPoolId: AuthStack.SpaceIdentityPoolId,
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+  },
+});
+
 export class AuthService {
+  private user: CognitoUser | undefined;
+
   public async login(userName: string, password: string): Promise<object | undefined> {
-    return {
-      user: 'abc',
-    };
+    try {
+      this.user = (await Auth.signIn(userName, password)) as CognitoUser;
+      return this.user;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   }
 
   public getUserName() {
-    return 'some user';
+    return this.user?.getUsername();
   }
 }
