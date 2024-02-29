6e1ad22a3b5f9779bd40937cd7ea023c4a8be6a8
eduard <avas.eduard@gmail.com>
2024-02-29 08:08:49 +0200

    updated the create space method with photo upload

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -19,7 +19,7 @@ Amplify.configure({
 
 export class AuthService {
   private user: CognitoUser | undefined;
-  private jwtToken: string | undefined;
+  public jwtToken: string | undefined;
   private temporaryCredentials: object | undefined;
 
   public async login(userName: string, password: string): Promise<object | undefined> {
